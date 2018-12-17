fcf1c6c6b23c71e99de7ac6a0ffd90afab828267
root <root@localhost.localdomain>
2018-12-17 19:58:36 +0530

    backend-setup-test: py test file
    Signed-off-by: Sheersha Jain <sheershajain@gmail.com>

--- roles/backend_setup/molecule/default/tests/test_default.py
+++ roles/backend_setup/molecule/default/tests/test_default.py
@@ -7,8 +7,23 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 
 def test_hosts_file(host):
-    f = host.file('/etc/hosts')
-
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+    cmd = host.run("vgdisplay")
+    for line in cmd.stdout.split(" "):
+        if "VG Name" in line:
+            assert "vg_vdb" in line
+            assert "vg_vdc" in line
+    cmd = host.run("lvdisplay")
+    for line in cmd.stdout.split(" "):
+        if "LV Path" in line:
+            assert "/dev/vg_vdc/vg_vdc_thinlv1" in line
+            assert "/dev/vg_vdc/vg_vdc_thinlv2" in line
+            assert "/dev/vg_vdb/thicklv_1" in line
+            assert "/dev/centos/swap" in line
+            assert "/dev/centos/root" in line
+        if "LV Name" in line:
+            assert "swap" in line
+            assert "root" in line
+            assert "foo_thinpool" in line
+            assert "vg_vdc_thinlv1" in line
+            assert "vg_vdc_thinlv2" in line
+            assert "thicklv_1" in line
